d6b693684bd059f86265e317d390c606b403d71c
yazan-hamamdi <yazan.hamamdi9@gmail.com>
2025-08-08 16:55:37 +0300

    Create project file

---
+++ Simple Inventory Management System/Simple Inventory Management System/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Simple_Inventory_Management_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
