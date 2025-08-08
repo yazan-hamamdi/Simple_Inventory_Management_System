4e89a7f67e9b01fa5ab66d154987da9f6476c28c
yazan-hamamdi <yazan.hamamdi9@gmail.com>
2025-08-08 17:15:09 +0300

    Create properties and Constructors

---
+++ Simple Inventory Management System/Simple Inventory Management System/Inventory .cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Inventory_Management_System
+{
+    public class Inventory
+    {
+        public List<Product> products { get; set; }
+        public Inventory(List<Product> products)
+        {
+            this.products = products;
+        }
+        public Inventory()
+        {
+            this.products = new List<Product>();
+        }
+    }
+}
---
+++ Simple Inventory Management System/Simple Inventory Management System/Product .cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Inventory_Management_System
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int quantity { get; set; }
+
+        public Product(string name, decimal price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            this.quantity = quantity;
+        }
+        public Product()
+        {
+       
+        }
+    }
+}
